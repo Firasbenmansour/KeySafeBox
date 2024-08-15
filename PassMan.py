1931084506df74b4f7d6049ae8b7e8da571a80fd
Firas Benmansour <132140694+Firasbenmansour@users.noreply.github.com>
2024-08-16 01:07:18 +0200

    Update PassMan.py

--- PassMan.py
+++ PassMan.py
@@ -1,6 +1,7 @@
 import pyfiglet
 from passwordGenerator import *
 from CreateDatabase import *
+from CheckDatabase import *
 
 def Banner():
     print("-----------------------------------------------")
@@ -16,14 +17,20 @@ print("Your trustworthy password manager at your service anytime you want!")
 print("1 - Create Database")
 print("2 - Check Databases")
 print("3 - Generate Password")
-choice = input("Enter a number (1-4): ")
-match choice:
-      case "1":
-          createDatabase()
-      case "2":
-          checkDatabase()
-      case "3":
-          generatePassword()
-      case _:
-          print("Please pick a valid option")
+while True:
+    choice = input("Enter a number (1-3): ")
+    if choice == "1":
+        print("-----------------------------------------------")
+        createDatabase()
+        break
+    elif choice == "2":
+        print("-----------------------------------------------")
+        enterDatabase()
+        break
+    elif choice == "3":
+        print("-----------------------------------------------")
+        generatePassword()
+        break
+    else:
+        print("Please pick a valid option")
 
