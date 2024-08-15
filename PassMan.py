b2377352b102bf6b142cf3994335148dfedae2c8
Firas Benmansour <132140694+Firasbenmansour@users.noreply.github.com>
2024-08-15 21:18:58 +0200

    Update PassMan.py

--- PassMan.py
+++ PassMan.py
@@ -1,8 +1,6 @@
 import pyfiglet
-import cryptography
-import random
-import string
 from passwordGenerator import *
+from CreateDatabase import *
 
 def Banner():
     print("-----------------------------------------------")
@@ -11,36 +9,20 @@ def Banner():
     print("-----------------------------------------------")
     return 1
 
-def databaseMenu():
-    print("1 - Stock Password")
-    print("2 - Check Password database")
-    choice=input()
-    match choice:
-          case "1":
-              Stock()
-          case "2":
-              checkDatabase()
-          case _:
-              print("Please pick a valid option")
-    
-
 
 ##### MAIN #####
 Banner()
 print("Your trustworthy password manager at your service anytime you want!")
 print("1 - Create Database")
-print("2 - Enter Database")
-print("3 - Delete Database")
-print("4 - Generate Password")
+print("2 - Check Databases")
+print("3 - Generate Password")
 choice = input("Enter a number (1-4): ")
 match choice:
       case "1":
           createDatabase()
       case "2":
-          enterDatabase()
+          checkDatabase()
       case "3":
-          deleteDatabase()
-      case "4":
           generatePassword()
       case _:
           print("Please pick a valid option")
