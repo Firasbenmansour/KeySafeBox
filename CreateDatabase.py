e65185c9ef5e2d1795a427ac4e5026b4918b4b20
Firas Benmansour <132140694+Firasbenmansour@users.noreply.github.com>
2024-08-17 20:32:59 +0200

    Update CreateDatabase.py

--- CreateDatabase.py
+++ CreateDatabase.py
@@ -1,34 +1,40 @@
-from encryptionDecryption import *
 import os
+import getpass
+
+from encryptionDecryption import crypt_csv
+from utils import secure_delete, refreshDatabaseList
+
+# Specify the directory path
+if not os.path.exists("Databases"):
+       os.makedirs("Databases")
+directory_path = "Databases"
+
+databases = os.listdir(directory_path)
 
 def createDatabase():
+    print("--------------------------------------------------------------")
     print("Database name:", end=' ')
     dbName = input().strip()
     while True:
-       print("Database password:", end=' ')
-       dbPass = input().strip()
-       print("Confirm password:", end=' ')
-       dbPassConfirm = input().strip()
+       dbPass = getpass.getpass("Enter your password: ").strip()
+       dbPassConfirm = getpass.getpass("Confirm password: ").strip()
        if dbPass != dbPassConfirm :
           print("Passwords do not match.")
        elif len(dbPass) < 8:
           print("Password must be at least 12 characters.")
        else:
           break
-
-    with open(dbName + ".csv", "x", newline='') as databaseFile:
+       
+    temp_file_path = os.path.join(directory_path, dbName + "_temp.csv")
+    with open(temp_file_path, "x", newline='') as databaseFile:
        databaseFile.write("Website,Username,Password\n")
        databaseFile.close()
 
+    encrypted_file_path = os.path.join(directory_path, dbName + ".csv")
+    crypt_csv(temp_file_path, encrypted_file_path, dbPass)
+    secure_delete(temp_file_path)
+    
+    refreshDatabaseList()
 
-    crypt_csv(dbName + ".csv", dbName + "_encrypted.csv", dbPass)
-    os.remove(dbName + ".csv")
-
-    # Ensure the "Databases" directory exists
-    if not os.path.exists("Databases"):
-       os.makedirs("Databases")
-
-    os.rename(dbName + "_encrypted.csv", "Databases/" + dbName + ".csv")
     print("Database created successfully.")
-
 
