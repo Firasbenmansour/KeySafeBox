1634abdfeab1c9c59f8b32100166c9c1b23d16e3
Firas Benmansour <132140694+Firasbenmansour@users.noreply.github.com>
2024-08-18 02:22:28 +0200

    Update CreateDatabase.py

--- CreateDatabase.py
+++ CreateDatabase.py
@@ -1,40 +1,38 @@
 import os
 import getpass
-
-from encryptionDecryption import crypt_csv
+import globals
 from utils import secure_delete, refreshDatabaseList
-
-# Specify the directory path
-if not os.path.exists("Databases"):
-       os.makedirs("Databases")
-directory_path = "Databases"
-
-databases = os.listdir(directory_path)
+from encryptionDecryption import crypt_csv
 
 def createDatabase():
     print("--------------------------------------------------------------")
-    print("Database name:", end=' ')
-    dbName = input().strip()
-    while True:
-       dbPass = getpass.getpass("Enter your password: ").strip()
-       dbPassConfirm = getpass.getpass("Confirm password: ").strip()
-       if dbPass != dbPassConfirm :
-          print("Passwords do not match.")
-       elif len(dbPass) < 8:
-          print("Password must be at least 12 characters.")
-       else:
-          break
-       
-    temp_file_path = os.path.join(directory_path, dbName + "_temp.csv")
-    with open(temp_file_path, "x", newline='') as databaseFile:
-       databaseFile.write("Website,Username,Password\n")
-       databaseFile.close()
+    dbName = input("Database name: ").strip()
+    dbNameCheck = dbName + ".csv"
 
-    encrypted_file_path = os.path.join(directory_path, dbName + ".csv")
+    while dbNameCheck in globals.databases:
+       print("Database already exists.")
+       dbName = input("Database name: ").strip()
+       dbNameCheck = dbName + ".csv"
+   
+    while True:
+        dbPass = getpass.getpass("Enter your password: ").strip()
+        dbPassConfirm = getpass.getpass("Confirm password: ").strip()
+        if dbPass != dbPassConfirm:
+            print("Passwords do not match.")
+        elif len(dbPass) < 8:
+            print("Password must be at least 8 characters.")
+        else:
+            break
+
+    temp_file_path = os.path.join(globals.directory_path, dbName + "_temp.csv")
+    with open(temp_file_path, "x", newline='') as databaseFile:
+        databaseFile.write("Website,Username,Password\n")
+
+    encrypted_file_path = os.path.join(globals.directory_path, dbName + ".csv")
     crypt_csv(temp_file_path, encrypted_file_path, dbPass)
     secure_delete(temp_file_path)
-    
+
+    # Refresh the global database list
     refreshDatabaseList()
 
     print("Database created successfully.")
-
